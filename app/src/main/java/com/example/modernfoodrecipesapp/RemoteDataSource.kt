0bf5b310f23aa333caa598bfeb1e1b83047ed032
pablohema <pablo_hm9@hotmail.com>
2021-02-21 18:20:51 +0100

    6.25 Inject RemoteDataSource.kt - Dagger/Hilt

--- app/src/main/java/com/example/modernfoodrecipesapp/RemoteDataSource.kt
+++ app/src/main/java/com/example/modernfoodrecipesapp/RemoteDataSource.kt
@@ -1,4 +1,14 @@
 package com.example.modernfoodrecipesapp
 
-class RemoteDataSource() {
+import com.example.modernfoodrecipesapp.models.FoodRecipe
+import retrofit2.Response
+import javax.inject.Inject
+
+class RemoteDataSource @Inject constructor(
+    private val foodRecipesApi: FoodRecipesApi
+) {
+
+    suspend fun getRecipes(queries: Map<String, String>): Response<FoodRecipe> {
+        return foodRecipesApi.getRecipes(queries)
+    }
 }

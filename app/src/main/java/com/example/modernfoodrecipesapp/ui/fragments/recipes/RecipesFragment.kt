7aff4a90f64ed864da6305d6edf92da8e23aa355
pablohema <pablo_hm9@hotmail.com>
2021-02-27 19:11:21 +0100

    13.60 Handle Return from BottomSheet - Safe Args

--- app/src/main/java/com/example/modernfoodrecipesapp/ui/fragments/recipes/RecipesFragment.kt
+++ app/src/main/java/com/example/modernfoodrecipesapp/ui/fragments/recipes/RecipesFragment.kt
@@ -10,6 +10,7 @@ import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.lifecycleScope
 import androidx.navigation.fragment.findNavController
+import androidx.navigation.fragment.navArgs
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.modernfoodrecipesapp.R
 import com.example.modernfoodrecipesapp.adapters.RecipesAdapter
@@ -24,6 +25,8 @@ import kotlinx.coroutines.launch
 
 @AndroidEntryPoint
 class RecipesFragment : Fragment() {
+
+    private val args by navArgs<RecipesFragmentArgs>()
 
     private var _binding: FragmentRecipesBinding? = null
     private val binding = _binding!!
@@ -70,7 +73,7 @@ class RecipesFragment : Fragment() {
             mainViewModel.readRecipes.observeOnce(
                 viewLifecycleOwner,
                 { database ->
-                    if (database.isNotEmpty()) {
+                    if (database.isNotEmpty() && !args.backFromBottomSheet) {
                         Log.d("RecipesFragment", "readDatabase called!")
                         mAdapter.setData(database[0].foodRecipe)
                         hideShimmerEffect()

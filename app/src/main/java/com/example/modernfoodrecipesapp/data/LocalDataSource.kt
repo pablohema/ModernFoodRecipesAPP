f631fd08b99d868cd0cffbb393f1b053ca6c491b
pablohema <pablo_hm9@hotmail.com>
2021-03-05 22:01:55 +0100

    19.87 Modify LocalDataSource.kt

--- app/src/main/java/com/example/modernfoodrecipesapp/data/LocalDataSource.kt
+++ app/src/main/java/com/example/modernfoodrecipesapp/data/LocalDataSource.kt
@@ -1,6 +1,7 @@
 package com.example.modernfoodrecipesapp.data
 
 import com.example.modernfoodrecipesapp.data.database.RecipesDao
+import com.example.modernfoodrecipesapp.data.database.entities.FavoritesEntity
 import com.example.modernfoodrecipesapp.data.database.entities.RecipesEntity
 import kotlinx.coroutines.flow.Flow
 import javax.inject.Inject
@@ -9,11 +10,27 @@ class LocalDataSource @Inject constructor(
     private val recipesDao: RecipesDao
 ) {
 
-    fun readDatabase(): Flow<List<RecipesEntity>> {
+    fun readRecipes(): Flow<List<RecipesEntity>> {
         return recipesDao.readRecipes()
+    }
+
+    fun readFavoriteRecipes(): Flow<List<FavoritesEntity>> {
+        return recipesDao.readFavoriteRecipes()
     }
 
     suspend fun insertRecipes(recipesEntity: RecipesEntity) {
         recipesDao.insertRecipes(recipesEntity)
+    }
+
+    suspend fun insertFavoriteRecipes(favoritesEntity: FavoritesEntity) {
+        return recipesDao.insertFavoriteRecipe(favoritesEntity)
+    }
+
+    suspend fun deleteFavoriteRecipes(favoritesEntity: FavoritesEntity) {
+        recipesDao.deleteFavoriteRecipe(favoritesEntity)
+    }
+
+    suspend fun deleteAllFavoriteRecipes() {
+        recipesDao.deleteAllFavoriteRecipes()
     }
 }

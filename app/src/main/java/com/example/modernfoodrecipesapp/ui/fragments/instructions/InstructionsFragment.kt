69ab5aa6d8cac5dc1cac2db4665a785d2283c2d1
pablohema <pablo_hm9@hotmail.com>
2021-03-05 21:26:07 +0100

    18.82 Modify InstructionsFragment.kt

--- app/src/main/java/com/example/modernfoodrecipesapp/ui/fragments/instructions/InstructionsFragment.kt
+++ app/src/main/java/com/example/modernfoodrecipesapp/ui/fragments/instructions/InstructionsFragment.kt
@@ -4,8 +4,12 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.webkit.WebViewClient
 import androidx.fragment.app.Fragment
 import com.example.modernfoodrecipesapp.R
+import com.example.modernfoodrecipesapp.models.Result
+import com.example.modernfoodrecipesapp.util.Constants
+import kotlinx.android.synthetic.main.fragment_instructions.view.*
 
 class InstructionsFragment : Fragment() {
 
@@ -15,6 +19,15 @@ class InstructionsFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_instructions, container, false)
+        val view = inflater.inflate(R.layout.fragment_instructions, container, false)
+
+        val args = arguments
+        val myBundle: Result? = args?.getParcelable(Constants.RECIPE_RESULT_KEY)
+
+        view.instructions_webview.webViewClient = object : WebViewClient() {}
+        val websiteUrl: String = myBundle!!.sourceUrl
+        view.instructions_webview.loadUrl(websiteUrl)
+
+        return view
     }
 }
